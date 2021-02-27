d18a1ec5c6872881af1e080945bdff57b7532594
yuxiao <2286229152@qq.com>
2021-02-27 11:00:25 +0800

    test 01

---
+++ crow_tests/crow_test01/main.cc
@@ -0,0 +1,26 @@
+#include "../crow_all.h"
+using std::cout;
+
+int main (int argc, char *argv[])
+{
+    crow::SimpleApp app;
+    
+    CROW_ROUTE(app, "/")([](const crow::request &req, crow::response &res){
+        std::ifstream in("../public/index.html", std::ifstream::in);
+        if (in) {
+            std::ostringstream contents;
+            contents << in.rdbuf();
+            in.close();
+            res.write(contents.str());
+        }
+        else {
+            res.write("Not found.");
+        }
+        res.end();
+    });
+
+    char *port = std::getenv("PORT");
+    uint16_t iPort = static_cast<uint16_t>(port != NULL ? std::stoi(port) : 18080);
+    cout << "PORT = " << iPort << std::endl;
+    app.port(iPort).multithreaded().run();
+}
